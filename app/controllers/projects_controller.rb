574d3ba83d7d7a64f4f2872ded0f0cf3440c335b
newton <newtonlaw18@outlook.com>
2016-11-27 16:30:37 +0800

    Add project page

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,49 @@
+class ProjectsController < ApplicationController
+	before_action :find_project, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@projects = Project.all.order("created_at desc")
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def show
+	end
+
+	def edit
+
+	end
+
+	def create 
+		@project = Project.new project_params
+		if @project.save
+			redirect_to @project, notice: "Project created"
+		else
+			render 'new', notice: "Failed"
+		end
+	end
+
+	def update
+		if @project.update project_params
+			redirect_to @project, notice: "Updated"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@project.destroy
+		redirect_to projects_path
+	end
+
+	private
+	def project_params
+		params.require(:project).permit(:title, :description, :link)
+	end
+
+	def find_project
+		@project = Project.find(params[:id])
+	end
+end
